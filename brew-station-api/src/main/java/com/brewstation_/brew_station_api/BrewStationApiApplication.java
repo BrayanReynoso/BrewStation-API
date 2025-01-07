c616ca88c75b28c14479950d89f6e7eff9da730b
Brayan Reynoso Macedo <20213tn084@utez.edu.mx>
2025-01-07 11:14:45 -0600

    [Add] Add project

---
+++ brew-station-api/src/main/java/com/brewstation_/brew_station_api/BrewStationApiApplication.java
@@ -0,0 +1,13 @@
+package com.brewstation_.brew_station_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewStationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewStationApiApplication.class, args);
+	}
+
+}
